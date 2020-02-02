476b204d0c548446bca733e619b056be5c6d4c66
dacicus75 <dacicus75@hotmail.com>
2020-02-02 15:04:26 -0500

    added array of cards

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container")
+const blogs = [
+  { name: "Blog1", image: "images/example-blog01.jpg" },
+  { name: "Blog2", image: "images/example-blog02.jpg" },
+  { name: "Blog3", image: "images/example-blog03.jpg" },
+  { name: "Blog4", image: "images/example-blog04.jpg" },
+  { name: "Blog5", image: "images/example-blog05.jpg" },
+  { name: "Blog6", image: "images/example-blog06.jpg" },
+  { name: "Blog7", image: "images/example-blog07.jpg" },
+  { name: "Blog8", image: "images/example-blog01.jpg" },
+  { name: "Blog9", image: "images/example-blog02.jpg" },
+]
+
