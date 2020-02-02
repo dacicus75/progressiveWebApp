0de9247a32e5f9837f4f40c6afc36efb66113f56
dacicus75 <dacicus75@hotmail.com>
2020-02-02 15:19:36 -0500

    added manifest.json file

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 const container = document.querySelector(".container")
 const blogs = [
-  { name: "Blog1", image: "images/example-blog01.jpg" },
-  { name: "Blog2", image: "images/example-blog02.jpg" },
-  { name: "Blog3", image: "images/example-blog03.jpg" },
-  { name: "Blog4", image: "images/example-blog04.jpg" },
-  { name: "Blog5", image: "images/example-blog05.jpg" },
-  { name: "Blog6", image: "images/example-blog06.jpg" },
-  { name: "Blog7", image: "images/example-blog07.jpg" },
-  { name: "Blog8", image: "images/example-blog01.jpg" },
-  { name: "Blog9", image: "images/example-blog02.jpg" },
+  { name: "Summer", image: "images/example-blog01.jpg" },
+  { name: "Study", image: "images/example-blog02.jpg" },
+  { name: "Winter", image: "images/example-blog03.jpg" },
+  { name: "Nights", image: "images/example-blog04.jpg" },
+  { name: "Concert", image: "images/example-blog05.jpg" },
+  { name: "Festival", image: "images/example-blog06.jpg" },
+  { name: "Fall", image: "images/example-blog07.jpg" },
+  { name: "Swimming", image: "images/example-blog01.jpg" },
+  { name: "Books", image: "images/example-blog02.jpg" },
 ]
 
 const showBlogs = () => {
